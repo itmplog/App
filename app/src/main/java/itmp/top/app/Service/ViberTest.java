9e80292a253d85d769f068a89cca03017e16c4cd
Law Yu <Law Yu>
2016-03-05 18:06:26 +0800

    ViberTest

---
+++ app/src/main/java/itmp/top/app/Service/ViberTest.java
@@ -0,0 +1,40 @@
+package itmp.top.app.Service;
+
+import android.app.Service;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import itmp.top.app.R;
+
+public class ViberTest extends AppCompatActivity {
+    Vibrator vibrator;
+    private RelativeLayout content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viber_test);
+        content = (RelativeLayout) findViewById(R.id.content);
+        vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//getWindow().getDecorView().getRootView()
+        Snackbar.make(content, "Viber ON", Snackbar.LENGTH_SHORT).setAction("Undone", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.cancel();
+            }
+        }).show();
+        vibrator.vibrate(2000);
+
+        return super.onTouchEvent(event);
+    }
+}
